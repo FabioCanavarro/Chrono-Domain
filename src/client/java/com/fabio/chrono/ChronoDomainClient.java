5471fbff7bb7431c539aa1248896412f0b1cd68c
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-04-02 17:30:22 +0700

    Initial commit

---
+++ src/client/java/com/fabio/chrono/ChronoDomainClient.java
@@ -0,0 +1,10 @@
+package com.fabio.chrono;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChronoDomainClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
