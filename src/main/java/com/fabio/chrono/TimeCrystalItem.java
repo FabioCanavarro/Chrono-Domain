9a11b7e339a99a714dbe049ee257e2908f49ef70
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-04-04 22:42:22 +0700

    Sound + particle + decretement of thingy

--- src/main/java/com/fabio/chrono/TimeCrystalItem.java
+++ src/main/java/com/fabio/chrono/TimeCrystalItem.java
@@ -4,8 +4,13 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class TimeCrystalItem extends Item {
     public TimeCrystalItem(Settings settings) {
@@ -14,7 +19,34 @@ public class TimeCrystalItem extends Item {
 
     @Override
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        World world = user.getWorld();
+
         ChronoDomain.registerTimeFieldEntity(entity, ChronoDomain.timefactor);
+        world.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
+                SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.PLAYERS,
+                1.0F, 1.0F);
+
+        if (world instanceof ServerWorld serverWorld) {
+            for (int i = 0; i < 20; i++) {
+                serverWorld.spawnParticles(
+                        ParticleTypes.PORTAL,
+                        entity.getX() + (world.getRandom().nextDouble() - 0.5) * entity.getWidth(),
+                        entity.getY() + world.getRandom().nextDouble() * entity.getHeight(),
+                        entity.getZ() + (world.getRandom().nextDouble() - 0.5) * entity.getWidth(),
+                        5,
+                        0,
+                        0.1,
+                        0,
+                        0.05
+                );
+            }
+        }
+
+        // Only consume the item in survival mode
+        if (!user.getAbilities().creativeMode) {
+            stack.decrement(1);
+        }
+
         return ActionResult.SUCCESS;
     }
 }
